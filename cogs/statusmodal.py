a504abd021c589bdf06ee03f21110a0988d0c49a
hi-im-andrew <andrew.mh.le@gmail.com>
2024-03-27 17:02:20 -0700

    switch to pycord & migrate status module to mongodb

--- cogs/statusmodal.py
+++ cogs/statusmodal.py
@@ -1,130 +1,159 @@
+import os
+import io
 import discord
 from discord.ext import commands
 from easy_pil import Editor, load_image_async, Font, Canvas
 from datetime import datetime
 from pytz import timezone
-import io
+from dotenv import load_dotenv
+from pymongo import MongoClient, ReturnDocument
 import sqlite3
 
+# connect to MongoDB cluster
+load_dotenv()
+CLIENT = MongoClient(os.environ["MONGODB_URI"])
 
-class StatusChangeModal(discord.ui.Modal):
-
-  def __init__(self):
-    super().__init__(title="Status Changer")
-    self.emStatus = discord.ui.TextInput(
-        label="Status",
-        min_length=1,
-        max_length=40,
-        required=True,
-        placeholder="short status description",
-        style=discord.TextStyle.short)
-    self.add_item(self.emStatus)
-    self.emBio = discord.ui.TextInput(label="Description",
-                                      min_length=1,
-                                      max_length=45,
-                                      required=True,
-                                      placeholder="short about me",
-                                      style=discord.TextStyle.short)
-    self.add_item(self.emBio)
-
-  async def on_submit(self, interaction: discord.Interaction):
-    timePST = timezone('US/Pacific')
-    timePSTnow = datetime.now(timePST)
-    time_str = timePSTnow.strftime('%m/%d/%Y %H:%M:%S %Z')
-    print(f"Status: {self.emStatus.value}")
-    print(f"Bio: {self.emBio.value}")
-    print(f"Bio: {time_str}")
-    await add_status(interaction.user.mention, self.emStatus.value,
-                     self.emBio.value, time_str)
-    await interaction.response.send_message(
-        "Your status is updated. Use <!status @user>", ephemeral=True)
+# initialize db collection
+DB = CLIENT["muwu_data"]
+USERS = DB["users"]
 
 
-class StatusModalCog(commands.Cog):
+class StatusUpdateModal(discord.ui.Modal):
 
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self):
+        super().__init__(title="Set a status")
+        self.emStatus = discord.ui.TextInput(
+            label="Status",
+            min_length=1,
+            max_length=40,
+            required=True,
+            placeholder="What's poppin?",
+            style=discord.TextStyle.short,
+        )
+        self.add_item(self.emStatus)
 
-  @commands.command(name="changestatus")
-  async def change_status(self, ctx):
-    modal = StatusChangeModal()
-    await ctx.send_modal(modal)
+    async def on_submit(self, interaction: discord.Interaction):
+        timePST = timezone("US/Pacific")
+        timePSTnow = datetime.now(timePST)
+        time_str = timePSTnow.strftime("%m/%d/%Y %H:%M:%S %Z")
+        await _set_status(interaction.user.id, self.emStatus.value, time_str)
+        await interaction.response.send_message(
+            discord.Embed(
+                description="Updated your status! Check your status with `!status`",
+                color=discord.Color.random,
+            ),
+            ephemeral=True,
+        )
+
+
+class BioUpdateModal(discord.ui.Modal):
+
+    def __init__(self):
+        super().__init__(title="Set a user bio")
+        self.emBio = discord.ui.TextInput(
+            label="Description",
+            min_length=1,
+            max_length=45,
+            required=True,
+            placeholder="About me",
+            style=discord.TextStyle.short,
+        )
+        self.add_item(self.emBio)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await _set_bio(interaction.user.id, self.emBio.value)
+        await interaction.response.send_message(
+            discord.Embed(
+                description="Updated your bio! Check your status with `!status`",
+                color=discord.Color.random,
+            ),
+            ephemeral=True,
+        )
+
+
+class Status(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="updatestatus")
+    async def updatestatus(self, ctx):
+        modal = StatusUpdateModal()
+        await ctx.send_modal(modal)
+
+    @commands.command(name="updatebio")
+    async def updatebio(self, ctx):
+        modal = BioUpdateModal()
+        await ctx.send_modal(modal)
+
+    @commands.command(aliases=["checkstatus", "getstatus"])
+    async def status(self, ctx, member: discord.Member = None):
+        if member is None:
+            await _create_status_banner(ctx, ctx.author, _get_status(ctx.author.id))
+        else:
+            await _create_status_banner(ctx, member, _get_status(member.id))
 
 
 async def setup(bot):
-  await bot.add_cog(StatusModalCog(bot))
+    await bot.add_cog(Status(bot))
 
 
 ##########################
-#DB Helper Functions
-async def add_status(user, status='', bio='', timestamp=''):
-  user = user[2:-1]
-  conn = sqlite3.connect('uwustatus.db')
-  c = conn.cursor()
-  c.execute('''
-    CREATE TABLE IF NOT EXISTS statuses (
-      user_id TEXT PRIMARY KEY,
-      Status TEXT,
-      Bio TEXT,
-      Timestamp TEXT
+# MONGODB HELPER FUNCTIONS
+
+
+def _set_status(user_id: int, status: str, timestamp: str) -> None:
+    USERS.find_one_and_update(
+        {"_id": user_id},
+        {"$set": {"status": status, "timestamp": timestamp}},
+        upsert=True,
     )
-  ''')
-  c.execute(
-      '''
-      INSERT INTO statuses (user_id, Status, Bio, Timestamp) 
-      VALUES (?, ?, ?, ?)
-      ON CONFLICT(user_id) 
-      DO UPDATE SET Status=excluded.Status, Bio=excluded.Bio, Timestamp=excluded.Timestamp''',
-      (user, status, bio, timestamp))
-  conn.commit()
-  conn.close()
-  return True
 
 
-async def get_status_data():
-  conn = sqlite3.connect('uwustatus.db')
-  c = conn.cursor()
-  c.execute('SELECT * FROM statuses')
-  statuses = {
-      row[0]: {
-          'Status': row[1],
-          'Bio': row[2],
-          'Timestamp': row[3]
-      }
-      for row in c.fetchall()
-  }
-  conn.close()
-  return statuses
+def _set_bio(user_id: int, bio: str) -> None:
+    USERS.find_one_and_update({"_id": user_id}, {"$set": {"bio": bio}}, upsert=True)
 
 
-async def statusbanner(text, member, user_data):
-  background = Editor("./imgs/sanrio.png")
-  poppins_large = Font.poppins(size=38)
-  poppins_medium = Font.poppins(size=30)
-  poppins_small = Font.poppins(size=25)
-  profile_picture = await load_image_async(str(member.display_avatar.url))
-  profile = Editor(profile_picture).resize((150, 150)).circle_image()
-  background.paste(profile, (30, 30))
-  background.ellipse((30, 30), 150, 150, outline="#e0c3a4", stroke_width=2)
-  background.text((200, 50),
-                  f"{member.display_name}",
-                  font=poppins_large,
-                  color="white")
-  background.text((200, 125),
-                  f"Status: {user_data['Status']}",
-                  font=poppins_medium,
-                  color="#FFFFFF")
-  background.text((200, 175),
-                  f"Bio: {user_data['Bio']}",
-                  font=poppins_medium,
-                  color="#FFFFFF")
-  background.rectangle((200, 100), width=350, height=2, fill="#FFFFFF")
-  background.text((200, 235),
-                  f"Last Modified: {user_data['Timestamp']}",
-                  font=Font.poppins(size=25),
-                  color="#FFFFFF")
-  with io.BytesIO() as image_binary:
-    background.image.save(image_binary, format="PNG")
-    image_binary.seek(0)
-    file = discord.File(fp=image_binary, filename="status_banner.png")
-    await text.send(file=file)
+def _get_status(user_id: int) -> dict:
+    query = USERS.find_one_and_update(
+        {"_id": user_id},
+        {"$setOnInsert": {"status": "", "bio": "No bio set.", "timestamp": "N/A"}},
+        upsert=True,
+        return_document=ReturnDocument.AFTER,
+    )
+    return {"status": query["status"], "bio": query["bio"], "timestamp": query["timestamp"]}
+
+
+async def _create_status_banner(ctx, member, user_data):
+    background = Editor("./imgs/sanrio.png")
+    poppins_large = Font.poppins(size=38)
+    poppins_medium = Font.poppins(size=30)
+    poppins_small = Font.poppins(size=25)
+    profile_picture = await load_image_async(str(member.display_avatar.url))
+    profile = Editor(profile_picture).resize((150, 150)).circle_image()
+    background.paste(profile, (30, 30))
+    background.ellipse((30, 30), 150, 150, outline="#e0c3a4", stroke_width=2)
+    background.text(
+        (200, 50), f"{member.display_name}", font=poppins_large, color="white"
+    )
+    background.text(
+        (200, 125),
+        f"Status: {user_data["status"]}",
+        font=poppins_medium,
+        color="#FFFFFF",
+    )
+    background.text(
+        (200, 175), f"Bio: {user_data["bio"]}", font=poppins_medium, color="#FFFFFF"
+    )
+    background.rectangle((200, 100), width=350, height=2, fill="#FFFFFF")
+    background.text(
+        (200, 235),
+        f"Last Modified: {user_data["timestamp"]}",
+        font=Font.poppins(size=25),
+        color="#FFFFFF",
+    )
+    with io.BytesIO() as image_binary:
+        background.image.save(image_binary, format="PNG")
+        image_binary.seek(0)
+        file = discord.File(fp=image_binary, filename="status_banner.png")
+        await ctx.reply(file=file)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-#module downloads; easy-pil, pytz
-#standard library imports
-#third party imports
+# module downloads; easy-pil, pytz
+# standard library imports
+# third party imports
 import os, discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-#local application imports
+# local application imports
 from cogs.welcome import Welcome
-from cogs.statusmodal import StatusChangeModal, get_status_data, statusbanner
-#import asyncpg
+from cogs.statusmodal import StatusUpdateModal  # , get_status_data, statusbanner
+
+# import asyncpg
 
 load_dotenv()
 
@@ -17,32 +18,34 @@ intents.members = True
 intents.message_content = True
 muwubot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @muwubot.event
 async def on_ready():
-  await muwubot.load_extension("cogs.welcome")
-  await muwubot.load_extension("cogs.statusmodal")
-  await muwubot.load_extension("cogs.gamble")
-  print("MuwuBot is Online")
+    await muwubot.load_extension("cogs.welcome")
+    await muwubot.load_extension("cogs.statusmodal")
+    await muwubot.load_extension("cogs.gamble")
+    print("MuwuBot is Online")
 
-@muwubot.tree.command(name="update_status",
-                      description="update your bio and status")
+
+@muwubot.tree.command(name="update_status", description="update your bio and status")
 async def update_status(interaction: discord.Interaction):
-  await interaction.response.send_modal(StatusChangeModal())
+    await interaction.response.send_modal(StatusUpdateModal())
 
-@muwubot.command(aliases=["checkstatus", "getstatus"])
-async def status(context, member: discord.Member = None):
-  if member is None:
-    await context.reply("Use format: <!status @user>")
-    return
-  users = await get_status_data()
-  user_id = str(member.id) #member.display_name
-  if user_id in users:
-      user_data = users[user_id]
-      await statusbanner(context, member, user_data)
-  else:
-      await context.reply(f"The status for {member.mention} is not available.")
+
+# @muwubot.command(aliases=["checkstatus", "getstatus"])
+# async def status(context, member: discord.Member = None):
+#  if member is None:
+#    await context.reply("Use format: <!status @user>")
+#    return
+#  users = await get_status_data()
+#  user_id = str(member.id) #member.display_name
+#  if user_id in users:
+#      user_data = users[user_id]
+#      await statusbanner(context, member, user_data)
+#  else:
+#      await context.reply(f"The status for {member.mention} is not available.")
 
 
 if __name__ == "__main__":
-  #load personal credentials
-  muwubot.run(os.environ['TOKEN'])
+    # load bot token
+    muwubot.run(os.environ["TOKEN"])
